7a2e50af085f0adaacbb21202fa495ad2b8f1b2a
Suja7 <sujanandhakumar@gmail.com>
2022-01-13 11:04:36 -0500

    modified file

--- Practice/Practice1.py
+++ Practice/Practice1.py
@@ -1,5 +1,6 @@
 thistuple = ("apple", "banana", "cherry")
 thistuple[1] = "blackcurrant"
 
-# the value is still the same:
+# the value is still the same: because tuple cannot modified
+
 print(thistuple)
